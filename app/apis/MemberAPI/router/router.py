86b644925994cef91a5fd6a2d722984644599561
waffle <nate.crema@gmail.com>
2024-12-04 21:27:33 +0900

    feat(MemberAPI): 'google로 로그인' router 보완 및 로그인상태 검증 router 추가 (/profile)

--- app/apis/MemberAPI/router/router.py
+++ app/apis/MemberAPI/router/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 
-from app.apis.MemberAPI.service.auth_service import GCPService
+from app.apis.MemberAPI.model import Member
+from app.apis.MemberAPI.service.auth_service import GCPService, WeaveAuthService
 from app.common.response.formatter import error_response, success_response
 
 router = APIRouter()
@@ -19,10 +20,23 @@ async def get_login_url():
 async def handle_login_redirect(request: Request):  
   try:
     gcp_token_info = GCPService.get_token(request)
-    univ_user_info = GCPService.get_user_depart(gcp_token_info['access_token'])
+    univ_member_info = GCPService.get_member_depart(gcp_token_info['access_token'])
 
-    return success_response(data=univ_user_info)
+    login_result = WeaveAuthService.login(univ_member_info, gcp_token_info)
+    if not login_result['result']:
+      raise Exception("Login Failed")
+    
+    return success_response(data={
+      "logined_member": login_result["member"],
+      "logined_token": login_result["jwt"],
+      "new_member": login_result["auto_joined"]
+    })
   except Exception as e:
     print(e)
     return error_response(str(e))
 
+@router.get("/profile")
+async def get_logined_info(current_user: Member.Member = Depends(WeaveAuthService.digest_token)):
+  print(current_user)
+  return success_response(data=current_user)
+  
