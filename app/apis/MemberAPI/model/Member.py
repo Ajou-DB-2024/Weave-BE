9a37dacfed03f6e810c7fb462950bbbf8d8e2243
waffle <nate.crema@gmail.com>
2024-12-04 21:25:13 +0900

    refactor(MemberAPI): model 명칭 재정의

--- app/apis/MemberAPI/model/Member.py
+++ app/apis/MemberAPI/model/Member.py
@@ -1,28 +1,39 @@
 from dataclasses import dataclass
 
-from app.apis.MemberAPI.model import MemberUnivInfo
-
 @dataclass
-class Member:
-  id: int
-  created_at: str
+class CommonMember:
+  """
+    공통 멤버타입 필드
+  """
   name: str
   email: str
-  phone: str
   major: str
 
 @dataclass
-class ExtendedMember(Member):
-  university: MemberUnivInfo
+class DBMember(CommonMember):
+  """
+    DB에 등록된 멤버가 가지고 있는 필드
+  """
+  id: int
+  created_at: str
+
+@dataclass
+class DBSavedMember(DBMember):
+  """
+    DB에 저장된 멤버정보가 가지고 있는 필드
+  """
+  major: str
 
 @dataclass
 class MemberUnivInfo:
   college: str
   department: str
   major: str
-
-@dataclass
-class UnivCourse:
-  type: str
   grade: str
 
+@dataclass
+class Member(DBMember):
+  """
+    서비스에서 사용되는 멤버객체의 타입
+  """
+  university: MemberUnivInfo
